93bc26d9ac1d2d681a66dd05576ee3324bfedce0
Soykan <gokcellkamal@gmail.com>
2022-03-19 11:10:36 +0300

    Added basic grid and building mechanic

---
+++ Assets/Scripts/GridBuildingSystem/BuildingSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSound : MonoBehaviour {
+
+    [SerializeField] private GridBuildingSystem2D gridBuildingSystem2D = null;
+    [SerializeField] private Transform pfBuildingSound = null;
+
+    private void Start() {
+
+        if (gridBuildingSystem2D != null) {
+            gridBuildingSystem2D.OnObjectPlaced += GridBuildingSystem2D_OnObjectPlaced;
+        }
+    }
+
+    private void GridBuildingSystem2D_OnObjectPlaced(object sender, System.EventArgs e) {
+        Transform buildingSoundTransform = Instantiate(pfBuildingSound, Vector3.zero, Quaternion.identity);
+        Destroy(buildingSoundTransform.gameObject, 2f);
+    }
+
+}
